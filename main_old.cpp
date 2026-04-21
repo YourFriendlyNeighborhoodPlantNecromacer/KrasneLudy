0902f050911b9390cc9920affb2e919487e1d574
Verwex <239588243+Zakierski@users.noreply.github.com>
2026-04-21 19:30:27 +0200

    raylib integration, check description for tutorial
    https://www.youtube.com/watch?v=m8sbsDM93tI

---
+++ main_old.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "GLUGLU/GLUGLU.h"
+#include "DataStructures/DataStructures.h"
+
+int main(){
+    std::cout << "Hello world!" << std::endl;
+    country farland;
+    farland.insert(new location);
+    farland.insert(new location);
+    farland.insert(new location);
+    farland.make_connection(0, 1);
+    farland.dp();
+
+    return 0;
+}
