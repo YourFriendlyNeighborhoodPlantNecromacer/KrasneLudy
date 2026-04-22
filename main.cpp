0e1c3adb0fd438604507d63270b1615f9dde150d
Verwex <239588243+Zakierski@users.noreply.github.com>
2026-04-22 14:29:53 +0200

    initial camera setup

--- main.cpp
+++ main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <raylib.h>
 
-using namespace std;
+#define FOV_ORTHOGRAPHIC 10.0f
 
 int main () {
-
-    const int SCREEN_WIDTH = 600;
-    const int SCREEN_HEIGHT = 400;
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 450;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "AiSD II - Projekt Krasnoludki");
     SetTargetFPS(60);
 
+    Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, FOV_ORTHOGRAPHIC, CAMERA_ORTHOGRAPHIC };
+
     while (WindowShouldClose() == false){
 
         BeginDrawing();
-            ClearBackground(BLACK);
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube((Vector3){-4.0f, 0.0f, 2.0f}, 2.0f, 5.0f, 2.0f, RED);
+                DrawCubeWires((Vector3){-4.0f, 0.0f, 2.0f}, 2.0f, 5.0f, 2.0f, GOLD);
+                DrawCubeWires((Vector3){-4.0f, 0.0f, -2.0f}, 3.0f, 6.0f, 2.0f, MAROON);
+
+                DrawSphere((Vector3){-1.0f, 0.0f, -2.0f}, 1.0f, GREEN);
+                DrawSphereWires((Vector3){1.0f, 0.0f, 2.0f}, 2.0f, 16, 16, LIME);
+
+                DrawCylinder((Vector3){4.0f, 0.0f, -2.0f}, 1.0f, 2.0f, 3.0f, 4, SKYBLUE);
+                DrawCylinderWires((Vector3){4.0f, 0.0f, -2.0f}, 1.0f, 2.0f, 3.0f, 4, DARKBLUE);
+                DrawCylinderWires((Vector3){4.5f, -1.0f, 2.0f}, 1.0f, 1.0f, 2.0f, 6, BROWN);
+
+                DrawCylinder((Vector3){1.0f, 0.0f, -4.0f}, 0.0f, 1.5f, 3.0f, 8, GOLD);
+                DrawCylinderWires((Vector3){1.0f, 0.0f, -4.0f}, 0.0f, 1.5f, 3.0f, 8, PINK);
+
+                DrawGrid(10, 1.0f);        // Draw a grid
+
+            EndMode3D();
+
+            DrawFPS(10, 10);
         EndDrawing();
     }
 
