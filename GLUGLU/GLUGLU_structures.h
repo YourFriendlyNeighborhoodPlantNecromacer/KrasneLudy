aeb8dd197a174ffdffda4c40fe607ddc7e15eaea
Verwex <239588243+Zakierski@users.noreply.github.com>
2026-04-22 19:47:10 +0200

    you forgot to push the removal of includes

--- GLUGLU/GLUGLU_structures.h
+++ GLUGLU/GLUGLU_structures.h
@@ -9,15 +9,5 @@
 #include "datastructures/linear/stacks/l_stack/l_stack.h"
 #include "datastructures/linear/dynamic_array/dynamic_array.h"
 #include "datastructures/linear/autosorted_dynamic_array/autosorted_dynamic_array.h"
-#include "datastructures/trees/n_tree.h"
-#include "datastructures/trees/b_tree.h"
-#include "datastructures/trees/bs_tree.h"
-#include "datastructures/trees/t_tree.h"
-#include "datastructures/trees/heap.h"
-#include "datastructures/matrixes/matrix/matrix.h"
-#include "datastructures/multidimentional/multidim.h"
-#include "datastructures/matrixes/connection_matrix/connection_martix_graph.h"
-#include "datastructures/matrixes/matrix_graph/matrix_graph.h"
-#include "datastructures/matrixes/adjacency_list/adjacency_list.h"
 
 #endif//__GLUGLU_STRUCTURES_H_
