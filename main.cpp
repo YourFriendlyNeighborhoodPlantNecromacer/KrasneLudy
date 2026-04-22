13d34d0b48692e389f7c091e944533bfb622b058
Verwex <239588243+Zakierski@users.noreply.github.com>
2026-04-22 14:36:03 +0200

    getting familiar with the 3d axis

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #define FOV_ORTHOGRAPHIC 10.0f
 
+/*
+AXIS REFERENCE:
+// positive x moves to right
+// positive y moves up
+// positive z moves closer
+*/
+
 int main () {
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 450;
@@ -20,20 +27,8 @@ int main () {
             BeginMode3D(camera);
 
                 DrawCube((Vector3){-4.0f, 0.0f, 2.0f}, 2.0f, 5.0f, 2.0f, RED);
-                DrawCubeWires((Vector3){-4.0f, 0.0f, 2.0f}, 2.0f, 5.0f, 2.0f, GOLD);
-                DrawCubeWires((Vector3){-4.0f, 0.0f, -2.0f}, 3.0f, 6.0f, 2.0f, MAROON);
 
-                DrawSphere((Vector3){-1.0f, 0.0f, -2.0f}, 1.0f, GREEN);
-                DrawSphereWires((Vector3){1.0f, 0.0f, 2.0f}, 2.0f, 16, 16, LIME);
-
-                DrawCylinder((Vector3){4.0f, 0.0f, -2.0f}, 1.0f, 2.0f, 3.0f, 4, SKYBLUE);
-                DrawCylinderWires((Vector3){4.0f, 0.0f, -2.0f}, 1.0f, 2.0f, 3.0f, 4, DARKBLUE);
-                DrawCylinderWires((Vector3){4.5f, -1.0f, 2.0f}, 1.0f, 1.0f, 2.0f, 6, BROWN);
-
-                DrawCylinder((Vector3){1.0f, 0.0f, -4.0f}, 0.0f, 1.5f, 3.0f, 8, GOLD);
-                DrawCylinderWires((Vector3){1.0f, 0.0f, -4.0f}, 0.0f, 1.5f, 3.0f, 8, PINK);
-
-                DrawGrid(10, 1.0f);        // Draw a grid
+                DrawGrid(10, 1.0f);
 
             EndMode3D();
 
