7bbefd3dd591909872ac480037c02be623253667
YourFriendlyNeighborhoodPlantNecromacer <LocalPlantNecromancer@gmail.com>
2026-04-29 14:52:31 +0200

    Test

--- DataStructures/datastructures.h
+++ DataStructures/datastructures.h
@@ -1,9 +1,7 @@
 #ifndef __DATASTRUCTUES_H__
 #define __DATASTRUCTUES_H__
 #include "country.h"
-/*
-
 #include "road.h"
 #include "location.h"
-*/
+
 #endif//__DATASTRUCTUES_H__
--- GLUGLU/datastructures/linear/stacks/l_stack/l_stack_definition.h
+++ GLUGLU/datastructures/linear/stacks/l_stack/l_stack_definition.h
@@ -1,27 +1,27 @@
-#ifndef __L_STACK_DEFFINITION_H__
-#include <iostream>
+#ifndef __STACK_DEFFINITION_H__
+#define __STACK_DEFFINITION_H__
+/*
+template <typename T>
+stack<T>::stack() : length(0){}
 
 template <typename T>
-l_stack<T>::l_stack() : length(0){}
-
+int64_t stack<T>::get_length(){return length;}
 template <typename T>
-int64_t l_stack<T>::get_length(){return length;}
-template <typename T>
-void l_stack<T>::append(T val){
+void stack<T>::append(T val){
     length++;
     stack<T>::append(val);
 }
 template <typename T>
-bool l_stack<T>::pop(){
+bool stack<T>::pop(){
     if(length == 0) return false;
     length--;
     return stack<T>::pop();
 }
 template <typename T>
-void l_stack<T>::dp(){
+void stack<T>::dp(){
     std::cout << "Lenght: " << length << std::endl;
     stack<T>::dp();
 }
+*/
 
-
-#endif // ___L_STACK_DEFFINITION_H__
+#endif//__STACK_DEFFINITION_H__
