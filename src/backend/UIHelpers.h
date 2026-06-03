5e62d1650f6b7bea37bb85eb9d5b90c93b65b33d
Verwex <239588243+Zakierski@users.noreply.github.com>
2026-06-03 11:54:32 +0200

    funkcje dźwiękowe i standardyzacja pobierania ścieżki

--- src/backend/UIHelpers.h
+++ src/backend/UIHelpers.h
@@ -5,16 +5,28 @@
 #include <src/Constants.h>
 
 namespace UI {
-    // Ładuje czcionkę z pełnym wsparciem to glifów polskich
+    // Zasoby audio UI
+    extern Sound hoverSound;
+    extern Sound selectSound;
+
+    // Funkcje do zarządzania dźwiękami UI
+    void LoadUISounds();
+    void UnloadUISounds();
+    void PlayHoverSound();
+    void PlaySelectSound();
+
+    inline const char* AssetPath(const char* subPath) {
+        return TextFormat("assets/%s", subPath);
+    }
+
     inline Font LoadStandardFont(int fontSize) {
         int codepoints[128] = { 0 };
         for (int i = 0; i < 95; i++) codepoints[i] = 32 + i;
         int polish[] = { 0x0104, 0x0105, 0x0106, 0x0107, 0x0118, 0x0119, 0x0141, 0x0142, 0x0143, 0x0144, 0x00D3, 0x00F3, 0x015A, 0x015B, 0x0179, 0x017A, 0x017B, 0x017C };
         for (int i = 0; i < 18; i++) codepoints[95 + i] = polish[i];
-        return LoadFontEx("fonts/Ranchers-Regular.ttf", fontSize, codepoints, 95 + 18);
+        return LoadFontEx(AssetPath("fonts/Ranchers-Regular.ttf"), fontSize, codepoints, 95 + 18);
     }
 
-    // Rysuje teksturę jako powtarzające się tło
     inline void DrawTiledBackground(Texture2D texture) {
         for (int x = 0; x < Config::SCREEN_WIDTH; x += texture.width) {
             for (int y = 0; y < Config::SCREEN_HEIGHT; y += texture.height) {
@@ -23,7 +35,6 @@ namespace UI {
         }
     }
 
-    // Rysuje standardowy przycisk menu
     inline void DrawMenuButton(Rectangle rec, const char* text, Font font) {
         bool hover = CheckCollisionPointRec(GetMousePosition(), rec);
         DrawRectangleRec(rec, hover ? DARKGRAY : GRAY);
